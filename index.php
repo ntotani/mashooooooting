5f78024108d9217a3d59f28e3a2615e854495ed5
Naoyuki TOTANI <naoyuki.totani@gree.net>
2012-10-19 14:58:44 +0900

    update data by cron

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $daily_ranking = file_get_contents('daily_ranking.json');
 $file = new SplFileObject("electric.csv");
+$electric = 3000;
 $hour = (date('H') - 1) . ':00';
 while (!$file->eof()) {
     $row = $file->fgetcsv();
@@ -13,21 +14,21 @@ while (!$file->eof()) {
 
 <!DOCTYPE html>
 <html>
-<head>
-<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-<style type="text/css">
-  html { height: 100% }
-  body { height: 100%; margin: 0px; padding: 0px }
-</style>
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-<script type="text/javascript" src="enchant.min.js"></script>
-<script type="text/javascript" src="main.js"></script>
-<script type="text/javascript">
-var daily_ranking = <?php echo $daily_ranking; ?>;
-var electric = <?php echo $electric; ?>;
-</script>
-</head>
-<body>
-  <div id="enchant-stage"></div>
-</body>
+    <head>
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+        <style type="text/css">
+            html { height: 100% }
+            body { height: 100%; margin: 0px; padding: 0px }
+        </style>
+        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+        <script type="text/javascript" src="enchant.min.js"></script>
+        <script type="text/javascript" src="main.js"></script>
+        <script type="text/javascript">
+            var daily_ranking = <?php echo $daily_ranking; ?>;
+            var electric = <?php echo $electric; ?>;
+        </script>
+    </head>
+    <body>
+        <div id="enchant-stage"></div>
+    </body>
 </html>
