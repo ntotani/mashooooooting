6ce01fa33825ea0794d90f959259ba86909287d0
Naoyuki TOTANI <naoyuki.totani@gree.net>
2012-10-24 18:36:57 +0900

    fix

--- js/level.coffee
+++ js/level.coffee
@@ -1,10 +1,10 @@
 window.level = [
-  (time:240, type:'enemy', x:100)
-  (time:90,  type:'enemy', x:500)
-  (time:90,  type:'enemy', x:100, w:48, h:48, icon:8, hp:3)
-  (time:30, type:'enemy', x:100)
-  (time:90,  type:'enemy', x:500, w:48, h:48, icon:8, hp:3)
-  (time:30, type:'enemy', x:500)
+  (time:240, type:'enemy', x:100, icon:6)
+  (time:90,  type:'enemy', x:500, icon:5)
+  (time:90,  type:'enemy', x:100, w:48, h:48, icon:4, hp:3)
+  (time:30, type:'enemy', x:100, icon:3)
+  (time:90,  type:'enemy', x:500, w:48, h:48, icon:2, hp:3)
+  (time:30, type:'enemy', x:500, icon:1)
   (time:30, type:'friend')
   (time:180, type:'boss', icon:0, w:124, h:124, hp:10)
 ]
--- js/level.js
+++ js/level.js
@@ -5,35 +5,39 @@
     {
       time: 240,
       type: 'enemy',
-      x: 100
+      x: 100,
+      icon: 6
     }, {
       time: 90,
       type: 'enemy',
-      x: 500
+      x: 500,
+      icon: 5
     }, {
       time: 90,
       type: 'enemy',
       x: 100,
       w: 48,
       h: 48,
-      icon: 8,
+      icon: 4,
       hp: 3
     }, {
       time: 30,
       type: 'enemy',
-      x: 100
+      x: 100,
+      icon: 3
     }, {
       time: 90,
       type: 'enemy',
       x: 500,
       w: 48,
       h: 48,
-      icon: 8,
+      icon: 2,
       hp: 3
     }, {
       time: 30,
       type: 'enemy',
-      x: 500
+      x: 500,
+      icon: 1
     }, {
       time: 30,
       type: 'friend'
